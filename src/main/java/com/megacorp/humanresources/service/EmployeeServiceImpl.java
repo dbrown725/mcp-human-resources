015602c8d3e8af65cb5658af8ee67a1464fb2dc2
dbrown725 <davidbrown772255@gmail.com>
2025-05-22 07:13:02 -0400

    Added logging and removed the not needed spring.jpa.database-platform property

--- src/main/java/com/megacorp/humanresources/service/EmployeeServiceImpl.java
+++ src/main/java/com/megacorp/humanresources/service/EmployeeServiceImpl.java
@@ -9,11 +9,18 @@ import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+
 //Annotation
 @Service
+@Slf4j
 
 //Class
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
@@ -27,6 +34,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	//Hack method? MCP Client/Agent/LLM for saveEmployee(Employee employee) always wanted to pass in an ID that it made up.
 	@Tool(name = "saveEmployeeUsingName", description = "Creates a new employee based on the passed name.")
 	public Employee saveEmployeeUsingName(String name) {
+		logger.debug("Enter saveEmployeeUsingName(String name): " + name);
 		Employee employee = new Employee();
 		employee.setName(name);
 		return employeeRepository.save(employee);
@@ -51,13 +59,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Tool(name = "fetchEmployeeList", description = "Get a list of all employees")
 	@Override
 	public List<Employee> fetchEmployeeList() {
+		logger.debug("Enter fetchEmployeeList()");
 		List<Employee> employeesList = (List<Employee>) employeeRepository.findAll();
 		return employeesList;
 	}
 
-	// Update operation
+	/**
+	 * Update an employee
+	 *
+	 * @param employee
+	 * @param employee id Long
+	 * @return Employee object
+	 */
+	@Tool(name = "updateEmployee", description = "Updates an existing employee.")
 	@Override
 	public Employee updateEmployee(Employee employee, Long employeeId) {
+		logger.debug("Enter updateEmployee(Employee employee:, Long employeeId) employee: " + employee + " employeeId: " + employeeId);
 		Employee depDB = employeeRepository.findById(employeeId).get();
 
 		if (Objects.nonNull(employee.getName()) && !"".equalsIgnoreCase(employee.getName())) {
@@ -76,6 +93,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	@Tool(name = "getEmployeeById", description = "Get a single employee by ID")
 	public Employee getEmployeeById(Long employeeId) {
+		logger.debug("Enter getEmployeeById(Long employeeId) employeeId: " + employeeId);
 		return employeeRepository.findById(employeeId).get();
 	}
 
@@ -88,6 +106,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	@Override
 	@Tool(name = "deleteEmployeeById", description = "Delete a single employee by ID")
 	public void deleteEmployeeById(Long employeeId) {
+		logger.debug("Enter deleteEmployeeById(Long employeeId) employeeId: " + employeeId);
 		employeeRepository.deleteById(employeeId);
 	}
 }
