dd1848ee3281a04a82b34bd33ba17d0717a9acb6
dbrown725 <davidbrown772255@gmail.com>
2025-05-30 20:17:56 -0400

    Clean up

--- src/main/java/com/megacorp/humanresources/entity/Employee.java
+++ src/main/java/com/megacorp/humanresources/entity/Employee.java
@@ -13,8 +13,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
