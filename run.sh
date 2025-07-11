83fcf37f806db3a3d20e369385f849777d9d7571
dbrown725 <davidbrown772255@gmail.com>
2025-07-10 20:30:07 -0400

    Clean up

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # to make executable on mac "chmod 755 YourScriptName.sh"
 # to run "./YourScriptName.sh"
 cd target
-exec /home/davidbrown/Documents/development/JDKs/openjdk-24_linux-x64_bin/jdk-24/bin/java -jar humanresources-0.0.1-SNAPSHOT.jar
+exec /home/YOUR_HOME_DIRECTORY/Documents/development/JDKs/openjdk-24_linux-x64_bin/jdk-24/bin/java -jar humanresources-0.0.1-SNAPSHOT.jar
