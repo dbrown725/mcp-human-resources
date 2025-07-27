bee204846ab92bbf40fe41d943f5ab1aea76bb95
dbrown725 <davidbrown772255@gmail.com>
2025-07-27 19:57:12 -0400

    Changed how the MANAGER_ID column is populated.

--- src/main/java/com/megacorp/humanresources/entity/Employee.java
+++ src/main/java/com/megacorp/humanresources/entity/Employee.java
@@ -11,6 +11,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import jakarta.persistence.SequenceGenerator;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 -- Employee data source: https://www.kaggle.com/datasets/williamlucas0/employee-sample-data
-INSERT INTO employee (EMPLOYEE_ID, MANAGER_ID, FIRST_NAME, LAST_NAME, TITLE, DEPARTMENT, BUSINESS_UNIT, gender, ethnicity, age, hire_Date, annual_Salary) SELECT EMPLOYEE_ID, MANAGER_ID, FIRST_NAME, LAST_NAME, TITLE, DEPARTMENT, BUSINESS_UNIT, gender, ethnicity, age, hire_Date, annual_Salary FROM CSVREAD('~/Documents/projects/mcp-human-resources/employee_data_load.csv');
+INSERT INTO employee (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, TITLE, DEPARTMENT, BUSINESS_UNIT, gender, ethnicity, age, hire_Date, annual_Salary) SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, TITLE, DEPARTMENT, BUSINESS_UNIT, gender, ethnicity, age, hire_Date, annual_Salary FROM CSVREAD('~/Documents/projects/mcp-human-resources/employee_data_load.csv');
+
+UPDATE EMPLOYEE t
+SET t.MANAGER_ID = (SELECT c.MANAGER_ID FROM CSVREAD('/home/davidbrown/Documents/projects/mcp-human-resources/employee_data_load.csv') c WHERE CAST(c.EMPLOYEE_ID AS BIGINT)= t.EMPLOYEE_ID)
+WHERE t.EMPLOYEE_ID IN (SELECT CAST(c.EMPLOYEE_ID AS BIGINT) FROM CSVREAD('/home/davidbrown/Documents/projects/mcp-human-resources/employee_data_load.csv') c);  
