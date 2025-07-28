0caacd4e25b657f69c710699f37694305e4a3e46
dbrown725 <davidbrown772255@gmail.com>
2025-07-27 20:03:43 -0400

    Clean up

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@
 INSERT INTO employee (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, TITLE, DEPARTMENT, BUSINESS_UNIT, gender, ethnicity, age, hire_Date, annual_Salary) SELECT EMPLOYEE_ID, FIRST_NAME, LAST_NAME, TITLE, DEPARTMENT, BUSINESS_UNIT, gender, ethnicity, age, hire_Date, annual_Salary FROM CSVREAD('~/Documents/projects/mcp-human-resources/employee_data_load.csv');
 
 UPDATE EMPLOYEE t
-SET t.MANAGER_ID = (SELECT c.MANAGER_ID FROM CSVREAD('/home/davidbrown/Documents/projects/mcp-human-resources/employee_data_load.csv') c WHERE CAST(c.EMPLOYEE_ID AS BIGINT)= t.EMPLOYEE_ID)
-WHERE t.EMPLOYEE_ID IN (SELECT CAST(c.EMPLOYEE_ID AS BIGINT) FROM CSVREAD('/home/davidbrown/Documents/projects/mcp-human-resources/employee_data_load.csv') c);  
+SET t.MANAGER_ID = (SELECT c.MANAGER_ID FROM CSVREAD('~/Documents/projects/mcp-human-resources/employee_data_load.csv') c WHERE CAST(c.EMPLOYEE_ID AS BIGINT)= t.EMPLOYEE_ID)
+WHERE t.EMPLOYEE_ID IN (SELECT CAST(c.EMPLOYEE_ID AS BIGINT) FROM CSVREAD('~/Documents/projects/mcp-human-resources/employee_data_load.csv') c);  
